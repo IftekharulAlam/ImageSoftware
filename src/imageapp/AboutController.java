10a1df0220b1959c400de9320060eec066e33773
Iftekharul Alam <iftekharulalam38@gmail.com>
2023-05-22 02:04:20 +0600

    About Added

--- src/imageapp/AboutController.java
+++ src/imageapp/AboutController.java
@@ -5,9 +5,17 @@
  */
 package imageapp;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +24,23 @@ import javafx.fxml.Initializable;
  */
 public class AboutController implements Initializable {
 
+    @FXML
+    private Button closeButton;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void closeButtonClicked(ActionEvent event) throws IOException {
+        // get a handle to the stage
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
+
 }
